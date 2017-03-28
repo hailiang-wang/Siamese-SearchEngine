e150e728b2960414386fb4571a06051b36667fee
Hai Liang Wang <hailiang.hl.wang@gmail.com>
2017-03-28 13:59:19 +0800

    traverse data

--- test/import.data.js
+++ test/import.data.js
@@ -2,9 +2,28 @@
  * Import Data into ElasticSearch
  */
 const test = require('ava')
+const config = require('../config/environment')
 const logger = require('../services/logging.service').getLogger('import.data')
+const _ = require('lodash')
+const topicData = require(config.root + '/data/data.topics.json')
 
 test('Import Data into ElasticSearch', async t => {
     logger.debug('test')
+    let topicKeys = _.keys(topicData)
+    for (let topicEleKey of topicKeys) {
+        logger.debug('key:', topicEleKey)
+        let topicEleData = topicData[topicEleKey]
+        let topicEleName = topicEleData.name
+        let topicEleQuestions = topicEleData.questions
+        let topicEleQuestionsKeys = _.keys(topicEleQuestions)
+        for (let topicEleQuestionsId of topicEleQuestionsKeys) {
+            // let topicEleQuestionsIdKeys = _.keys(topicEleQuestions[topicEleQuestionsId])
+            logger.debug('topicEleQuestionsId', topicEleQuestionsId)
+            for (let topicEleQuestionsVar of topicEleQuestions[topicEleQuestionsId]) {
+                logger.debug('topicEleQuestionsVar.questionId', topicEleQuestionsVar.questionId)
+            }
+        }
+    }
+    logger.debug('passed.')
     t.pass()
 })
