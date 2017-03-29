03eca554a48b1e1d48b6522ae7a641d5c756eb69
Hai Liang Wang <hailiang.hl.wang@gmail.com>
2017-03-29 15:27:13 +0800

    Add more testcase

--- test/siamese.test.js
+++ test/siamese.test.js
@@ -9,16 +9,16 @@ const test = require('ava')
 /**
  * Should resolve
  */
-test.only('Siamese Search#Nouns what is your name', async t => {
+test.skip('Siamese Search#Nouns what is your name', async t => {
     let result = await siameseService.searchWithPOSNouns('topic', 'question', 'what is your name');
     console.log('result(what is your name):', JSON.stringify(result))
     t.pass()
 })
-    
+
 /**
  * Should resolve 
  */
-test.only('Siamese Search#Nouns would I know your name', async t => {
+test.skip('Siamese Search#Nouns would I know your name', async t => {
     let result = await siameseService.searchWithPOSNouns('topic', 'question', 'would I know your name');
     console.log('result(would I know your name):', JSON.stringify(result))
     t.pass()
@@ -30,6 +30,24 @@ test.only('Siamese Search#Nouns would I know your name', async t => {
 test.skip('Siamese Search#Nouns would I know your nickname', async t => {
     let result = await siameseService.searchWithPOSNouns('topic', 'question', 'would I know your nickname');
     console.log('result(would I know your nickname):', JSON.stringify(result))
+    t.pass()
+})
+
+/**
+ * Should resolve
+ */
+test.only('Siamese Search#Nouns What is your favorite animal', async t => {
+    let result = await siameseService.searchWithPOSNouns('topic', 'question', 'What is your favorite animal');
+    console.log('result(What is your favorite animal):', JSON.stringify(result))
+    t.pass()
+})
+
+/**
+ * Should resolve
+ */
+test.only('Siamese Search#Nouns What is your favorite animal', async t => {
+    let result = await siameseService.searchWithPOSNouns('topic', 'question', 'What is favorite animal');
+    console.log('result(What is your favorite animal):', JSON.stringify(result))
     t.pass()
 })
 
